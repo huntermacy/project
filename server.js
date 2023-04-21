1c8410f01fdd586a348e871cfbad89867ceab858
huntermacy <122992463+huntermacy@users.noreply.github.com>
2023-04-21 21:40:33 +0000

    Main

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.listen(3300, () => console.log('Console listening at 3300'))
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/').then(() => {
+mongoose.connect('mongodb://localhost:27017/test').then(() => {
     console.log('Connected to database');
 }).catch((err) => {
     console.log(`Error connecting to database: ${err}`);
