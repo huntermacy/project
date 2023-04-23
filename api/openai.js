1a9b59bd334da7e7445cbee26a9bdb975617725f
huntermacy <122992463+huntermacy@users.noreply.github.com>
2023-04-23 15:12:47 +0000

    Information entered by user --> parsed by OpenAI --> Stored in databse

--- api/openai.js
+++ api/openai.js
@@ -1,5 +1,12 @@
+const dotenv = require('dotenv')
+
 const express = require("express");
 const router = express.Router();
+
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -18,8 +25,28 @@ router.post("/", async (req, res) => {
         function generatePrompt(workout) {
             return `Return JSON {"reps":, "sets":, "weight":, "type":} from ${workout} with type being the type of workout`;
         }
-        console.log(response.data.choices[0].text)
-        res.json({ "message": response.data.choices[0].text });
+        try {
+            const workoutJSON = JSON.parse(response.data.choices[0].text);
+            async function run() {
+                try {
+                    await client.connect();
+                    await client.db("admin").command({ ping: 1 });
+                    console.log("Successfully connected to DB");
+                    const db = client.db("test");
+                    const collection = db.collection("workouts");
+                    await collection.insertOne(workoutJSON);
+                    console.log("Workout inserted into collection!");
+                } catch (err) {
+                    console.error(err);
+                } finally {
+                    await client.close();
+                }
+            }
+            run().catch(console.dir);
+            res.send(workoutJSON)
+        } catch {
+            console.log("Error Parsing JSON")
+        };
     } catch (error) {
         if (error.response) {
             console.error(error.response.status, error.response.data);
--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 const openAiGet = require("./api/openai")
 app.use("/api/openai", openAiGet)
 
-//Serve html page to the client
+//Serve index.html page to the client
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/pages/index.html");
 })
+//Serve stats.html page to the client
+app.get("/stats", (req, res) => {
+    res.sendFile(__dirname + "/pages/stats.html");
+})
+
 
 //Server starts listening on PORT
 app.listen(PORT, () => console.log(`Console listening at ${PORT}`))
@@ -31,51 +36,3 @@ app.options("*", (req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Authorization, Content-Length, X-Requested-With');
     res.send(200);
 });
-
-// Connect to MongoDB
-const { MongoClient } = require('mongodb');
-const uri = MONGO_URI;
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Successfully connected to DB");
-
-        // create a new schema for workouts
-        const workoutSchema = {
-            users: {
-                workout: {
-                    type: String,
-                    stats: {
-                        reps: {
-                            type: Number
-                        },
-                        sets: {
-                            type: Number
-                        },
-                        weight: {
-                            type: Number
-                        },
-                        timestamp: {
-                            type: Number
-                        }
-                    },
-                }
-            }
-        };
-
-        // create a new collection with the workout schema
-        const db = client.db("test");
-        const collection = db.collection("workouts");
-        // await collection.insertOne(workoutSchema);
-        console.log("Workout schema created!");
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
